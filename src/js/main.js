8e8b15c1668e08536115730d47b5f5eb6499d99b
Anton <antemionoy@gmail.com>
2018-10-08 18:28:19 +0300

    mod cards goods

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,7 @@
 //= ../../bower_components/jquery/dist/jquery.min.js
 //= ../../bower_components/owl.carousel/dist/owl.carousel.js
+//= ../../bower_components/select2/dist/js/select2.min.js
+
 
 "use strict";
 
@@ -33,6 +35,76 @@ function owlFunction(carousel) {
 }
 
 
+function quantity() {
+
+    $('<div class="quantity__nav"><div class="quantity__button quantity__button_up">+</div><div class="quantity__button quantity__button_down">-</div></div>').insertAfter('.quantity input');
+    $('.quantity').each(function() {
+        var spinner = $(this),
+            input = spinner.find('input[type="number"]'),
+            btnUp = spinner.find('.quantity__button_up'),
+            btnDown = spinner.find('.quantity__button_down'),
+            min = input.attr('min'),
+            max = input.attr('max');
+
+        btnUp.click(function() {
+            var oldValue = parseFloat(input.val());
+            if (oldValue >= max) {
+                var newVal = oldValue;
+            } else {
+                var newVal = oldValue + 1;
+            }
+            spinner.find("input").val(newVal);
+            spinner.find("input").trigger("change");
+        });
+
+        btnDown.click(function() {
+            var oldValue = parseFloat(input.val());
+            if (oldValue <= min) {
+                var newVal = oldValue;
+            } else {
+                var newVal = oldValue - 1;
+            }
+            spinner.find("input").val(newVal);
+            spinner.find("input").trigger("change");
+        });
+
+    });
+
+}
+
+function cardBsuket(){
+
+    $('.catalog__basket').on('click', function() {
+
+        $('.c-backet__inner').removeClass('c-backet__inner_open');
+        $(this).children('.c-backet__inner').addClass('c-backet__inner_open');
+
+    });
+
+}
+
+
+
+function customSelect() {
+
+    $('.c-price__select').select2({
+        minimumResultsForSearch: -1,
+        width: 51
+    });
+
+    $('.c-using__select').select2({
+        minimumResultsForSearch: -1,
+        width: 70
+    });
+
+}
+
+
 $(function() {
+
+    cardBsuket();
     owlFunction('.card__carousel');
+    quantity();
+    customSelect();
+
 });
